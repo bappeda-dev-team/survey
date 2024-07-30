e025ebaa6f0674436ef7b58589bebb736132c6c2
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-07-30 11:22:24 +0700

    user auth dan logo kota

--- lib/survey_web/router.ex
+++ lib/survey_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SurveyWeb.Router do
   use SurveyWeb, :router
 
+  import SurveyWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule SurveyWeb.Router do
     plug :put_root_layout, html: {SurveyWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -17,15 +20,6 @@ defmodule SurveyWeb.Router do
   scope "/", SurveyWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
-
-    # penduduks
-    live "/penduduks", PendudukLive.Index, :index
-    live "/penduduks/new", PendudukLive.Index, :new
-    live "/penduduks/:id/edit", PendudukLive.Index, :edit
-
-    live "/penduduks/:id", PendudukLive.Show, :show
-    live "/penduduks/:id/show/edit", PendudukLive.Show, :edit
     # end
   end
 
@@ -48,6 +42,54 @@ defmodule SurveyWeb.Router do
 
       live_dashboard "/dashboard", metrics: SurveyWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", SurveyWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{SurveyWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", SurveyWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SurveyWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      get "/", PageController, :home
+
+      # penduduks
+      live "/penduduks", PendudukLive.Index, :index
+      live "/penduduks/new", PendudukLive.Index, :new
+      live "/penduduks/:id/edit", PendudukLive.Index, :edit
+
+      live "/penduduks/:id", PendudukLive.Show, :show
+      live "/penduduks/:id/show/edit", PendudukLive.Show, :edit
+    end
+  end
+
+  scope "/", SurveyWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{SurveyWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
